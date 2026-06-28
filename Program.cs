434a46d25ced1910917a013be9d5915402d16af8
Santi <santiagod.sddo@gmail.com>
2026-06-28 17:59:34 -0400

    experimenting with first health endpoint

--- Program.cs
+++ Program.cs
@@ -1,41 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+static void Main(string[] args)
 {
-    app.MapOpenApi();
+    var builder = WebApplication.CreateBuilder(args);
+
+    var app = builder.Build();
+
+    app.MapGet("/health", Health);
+    //app.MapGet("/other", other);
+    //app.MapGet("/another", anoter);
+    //app.MapGet("/someother", someother);
+
+    app.Run();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
+static IResult Health()
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    return Results.Ok("Server Running");
 }
+
+Main([]);
