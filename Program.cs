588b93bb3aa08d098b7c8cc704ef9cbc5d912954
Santi <santiagod.sddo@gmail.com>
2026-06-28 18:11:09 -0400

    fixing nonsense

--- Program.cs
+++ Program.cs
@@ -1,20 +1,12 @@
-static void Main(string[] args)
-{
-    var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-    var app = builder.Build();
+var app = builder.Build();
 
-    app.MapGet("/health", Health);
-    //app.MapGet("/other", other);
-    //app.MapGet("/another", anoter);
-    //app.MapGet("/someother", someother);
+app.MapGet("/health", Health);
 
-    app.Run();
-}
+app.Run();
 
 static IResult Health()
 {
     return Results.Ok("Server Running");
 }
-
-Main([]);
